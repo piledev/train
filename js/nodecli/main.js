3f4ed230365bca746b3fe240c889b195cfe9274d
yokoyamada <pile.dev@gmail.com>
2021-03-01 09:19:24 +0900

    update nodecli

--- js/nodecli/main.js
+++ js/nodecli/main.js
@@ -1,10 +1,12 @@
 const program = require('commander');
 const fs = require('fs');
-const marked = require('marked');
+const md2html = require('./md2html');
 
 // GFMオプションを定義する
 program.option('--gfm', 'GFMを有効にする');
 program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
 
 // gfm: false をデフォルトオプションとする
@@ -14,8 +16,6 @@ const cliOptions = {
   ...program.opts(),
 };
 
-// ファイルパスをprogram.args配列から取り出す
-
 fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
   if (err) {
     console.error(err.message);
@@ -23,8 +23,6 @@ fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
     return;
   }
 
-  const html = marked(file, {
-    gfm: cliOptions.gfm,
-  });
+  const html = md2html(file, cliOptions);
   console.log(html);
 });
---
+++ js/nodecli/test/fixtures/sample.md
@@ -0,0 +1,8 @@
+# sample
+
+this is sample markdown
+https://jsprimer.net/
+
+- sample1
+- sample2
+- sample3
