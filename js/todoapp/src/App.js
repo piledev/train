b04746819d4398351d8ebbd38c97d2484241e029
yokoyamada <pile.dev@gmail.com>
2021-03-09 09:13:16 +0900

    refactoring 2: add handle functions

--- js/todoapp/src/App.js
+++ js/todoapp/src/App.js
@@ -3,10 +3,35 @@ import { TodoItemModel } from './model/TodoItemModel.js';
 import { TodoListView } from './view/TodoListView.js';
 import { render } from './view/html-util.js';
 
+// App の仕事はModelとView間のイベントを管理するだけ
 export class App {
   constructor() {
-    // 1. TodoListModelの初期化
+    this.todoListView = new TodoListView();
     this.todoListModel = new TodoListModel();
+  }
+
+  /**
+   * Todoを追加するときに呼ばれるリスナー関数
+   * @param {string} title
+   */
+  handleAdd(title) {
+    this.todoListModel.addTodo(new TodoItemModel({ title, completed: false }));
+  }
+
+  /**
+   * Todoの状態を更新したときに呼ばれるリスナー関数
+   * @param {{ id:number, completed: boolean }}
+   */
+  handleUpdate({ id, completed }) {
+    this.todoListModel.updateTodo({ id, completed });
+  }
+
+  /**
+   * Todoを削除したときに呼ばれるリスナー関数
+   * @param {{ id: number }}
+   */
+  handleDelete({ id }) {
+    this.todoListModel.deleteTodo({ id });
   }
 
   mount() {
@@ -26,10 +51,12 @@ export class App {
       //todoItemsに対応するTodoListViewを作成する
       const todoListElement = todoListView.createElement(todoItems, {
         onUpdateTodo: ({ id, completed }) => {
-          this.todoListModel.updateTodo({ id, completed });
+          // this.todoListModel.updateTodo({ id, completed });
+          this.handleUpdate({ id, completed });
         },
         onDeleteTodo: ({ id }) => {
-          this.todoListModel.deleteTodo({ id });
+          // this.todoListModel.deleteTodo({ id });
+          this.handleDelete({ id });
         },
       });
       // containerElementの中身をtodoListElementで上書きする
@@ -45,12 +72,13 @@ export class App {
       // 本来のsubmitイベントの動作を止める
       event.preventDefault();
       // 新しいTodoItemをTodoListへ追加する
-      this.todoListModel.addTodo(
-        new TodoItemModel({
-          title: inputElement.value,
-          completed: false,
-        }),
-      );
+      // this.todoListModel.addTodo(
+      //   new TodoItemModel({
+      //     title: inputElement.value,
+      //     completed: false,
+      //   }),
+      // );
+      this.handleAdd(inputElement.value);
       inputElement.value = '';
       console.log(`formElement's submit event has finished.`);
     });
